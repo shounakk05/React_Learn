bf0aff1a0592c4829e1e6e5e22f95bd3ec3dce3d
shounak karandikar <shounakk05@gmail.com>
2025-11-18 17:06:59 +0530

    Initial commit

---
+++ 01_Folder/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id = "parent">
+        <h1>This comes from react</h1>
+      </div>
+      <div id = "relative">
+        <h1>This dosen't come from react</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
